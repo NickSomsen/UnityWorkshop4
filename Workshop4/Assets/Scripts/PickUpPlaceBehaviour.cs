6e3c108e6a5f8d09c9c961f357d843289b52f77e
NickSomsen <nicksomsen@gmail.com>
2022-09-29 11:25:39 +0200

    bridge move

--- Workshop4/Assets/Scripts/PickUpPlaceBehaviour.cs
+++ Workshop4/Assets/Scripts/PickUpPlaceBehaviour.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PickUpPlaceBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private GameObject[] bridgePieces;
+    private int bridgePieceIdx = 0;
 
-    // Update is called once per frame
-    void Update()
+    public float bridgeHeight;
+
+    private void Start()
     {
-        
+        bridgePieces = GameObject.FindGameObjectsWithTag("BridgePiece");
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("PickUp"))
+        {
+            Debug.Log(bridgePieceIdx);
+            GameObject bridgePiece = bridgePieces[bridgePieceIdx];
+            bridgePiece.transform.position += new Vector3(0.0f, bridgeHeight * Time.deltaTime, 0.0f);
+            bridgePieceIdx++;
+        }
     }
 }
