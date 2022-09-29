556c9bd3f6cb5064fa8f93977ef6e18f6f60a21a
NickSomsen <nicksomsen@gmail.com>
2022-09-29 10:14:13 +0200

    player movement

---
+++ Workshop4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float speed;
+    private float horizontalMovement;
+    private float verticalMovement;
+
+    public float jumpHeight;
+    private bool isGrounded = true;
+    private bool isJumpPressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(horizontalMovement, 0.0f, verticalMovement);
+        rb.AddForce(movement * speed);
+
+        if (isJumpPressed && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+            isJumpPressed = false;
+            isGrounded = false;
+        }
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        horizontalMovement = movementVector.x;
+        verticalMovement = movementVector.y;
+    }
+
+    void OnJump()
+    {
+        if (isGrounded) isJumpPressed = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+}
